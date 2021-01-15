0dc45fada2646e4f25c8d59ef9214f27d4ed68d9
CathyZou <guangxia.zou@zatech.com>
2021-01-15 17:02:13 +0800

    rxjs document

--- src/Rxjs/code/operators/conversion/mapTo.tsx
+++ src/Rxjs/code/operators/conversion/mapTo.tsx
@@ -8,7 +8,10 @@ const Index: React.FC = props => {
   useEffect(() => {
     // 例1：每2秒发出一个值， 将所有发出值映射成同一个值
     const source = interval(1000).pipe(mapTo('HELLO WORLD!'));
-    source.subscribe(val => setHtml('mapToOutput', val));
+    const subscription = source.subscribe(val => setHtml('mapToOutput', val));
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 1000);
     // 例2： 将点击映射成字符串
     // const source2 = fromEvent(document, 'click').pipe(mapTo('GOODBYE WORLD!'));
     // source2.subscribe(val => setHtml('mapToOutput', val));
